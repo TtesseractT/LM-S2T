773080633de2ddc2358d2898dfa043952341b95c
TtesseractT <sabian.hibbs@gmail.com>
2023-05-29 18:26:45 +0100

    Remove val_cleanup.py from origin

--- val_cleanup.py
+++
@@ -1,4 +0,0 @@
-with open('validated.tsv', 'r') as input_file, open('output_file.tsv', 'w') as output_file:
-    for line in input_file:
-        new_line = line.replace('.mp3', '.wav')
-        output_file.write(new_line)
